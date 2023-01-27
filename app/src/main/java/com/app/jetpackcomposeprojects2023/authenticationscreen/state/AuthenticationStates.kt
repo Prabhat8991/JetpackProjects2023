3ce874eb8c7286c82228d694a3f4a2a6f5fe42ba
Prabhat <singh.prabhat8991@gmail.com>
2023-01-27 10:27:01 +0530

    added validation logic

--- app/src/main/java/com/app/jetpackcomposeprojects2023/authenticationscreen/state/AuthenticationStates.kt
+++ app/src/main/java/com/app/jetpackcomposeprojects2023/authenticationscreen/state/AuthenticationStates.kt
@@ -3,12 +3,20 @@ package com.app.jetpackcomposeprojects2023.authenticationscreen.state
 import androidx.annotation.StringRes
 import com.app.jetpackcomposeprojects2023.R
 
-data class AuthenticationState(
+data class AuthenticationState (
     val authenticationMode: AuthenticationMode = AuthenticationMode.SIGN_IN,
     val emailAddress: String?,
     val password: String?,
-    val passwordRequirements: List<PasswordRequirement> = emptyList()
-)
+    val passwordRequirements: List<PasswordRequirement> = emptyList(),
+    val isLoading: Boolean,
+    val error: String?
+) {
+    fun isValidState(): Boolean {
+        return emailAddress?.isNotEmpty() == true && password?.isNotEmpty() == true && (authenticationMode == AuthenticationMode.SIGN_IN ||
+                passwordRequirements.containsAll(PasswordRequirement.values().toList()))
+    }
+}
+
 
 enum class PasswordRequirement(
     @StringRes message: Int
